c35dd525e82aa67c41ae4343b6590bad6a065cee
adham-66 <adhamhewalla66@gmail.com>
2023-03-04 16:14:47 +0200

    add cart details app

--- project/urls.py
+++ project/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from apps.core.views import frontpage, contact, about
 from apps.store.views import product_detail, category_detail
+from apps.cart.views import cart_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', frontpage, name='frontpage'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
+    path('cart/', cart_detail, name='cart'),
     path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail'),
     path('<slug:slug>/', category_detail, name='category_detail'),
 ]
